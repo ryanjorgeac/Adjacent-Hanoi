05e5b941a9d5b572bd1385d019b4d99cf4ca1caa
ryanjorgeac <95293822+ryanjorgeac@users.noreply.github.com>
2022-01-04 11:09:00 -0300

    Add files via upload

---
+++ adjacent-hanoi.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+#pin1 --> start pin
+#pin2 --> auxiliary pin
+#pin3 --> destination pin
+
+def move(pin1,pin3):
+  print("{}->{}".format(pin1,pin3))
+
+def hanoi(n,pin1,pin2,pin3):
+    if n==0:
+      pass
+    else:
+      hanoi(n-1,pin1,pin2,pin3)
+      move(pin1,pin2)
+      hanoi(n-1,pin3,pin2,pin1)
+      move(pin2,pin3)
+      hanoi(n-1,pin1,pin2,pin3)
+quantPin = int(input("How many disks do you want to solve?"))
+hanoi(quantPin,1,2,3)
